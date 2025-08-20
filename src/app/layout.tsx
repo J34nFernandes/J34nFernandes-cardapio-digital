b0ded7b62e986097120ecfe05ece034fa7c3e887
J34nFernandes <jean@idetec.com.br>
2025-08-20 15:16:46 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
-import { ThemeProvider } from "@/components/theme-provider";
-import { cn } from "@/lib/utils";
+import { Toaster } from "../components/ui/toaster";
+import { ThemeProvider } from "../components/theme-provider";
+import { cn } from "../lib/utils";
 import { Inter, Space_Grotesk } from 'next/font/google'
-import { CartProvider } from "@/hooks/use-cart";
-import { AuthProvider } from "@/hooks/use-auth";
-import { AppSettingsProvider } from "@/hooks/use-app-settings";
+import { CartProvider } from "../hooks/use-cart";
+import { AuthProvider } from "../hooks/use-auth";
+import { AppSettingsProvider } from "../hooks/use-app-settings";
 
 
 const fontBody = Inter({
